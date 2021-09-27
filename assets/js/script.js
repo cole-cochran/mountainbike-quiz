450e68d8db759ef9d8d1211179f36350a47442e7
Cole Cochran <colecochran405@gmail.com>
2021-09-27 18:50:03 -0500

    creating timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 //WHEN I click the start button
 var startButton = document.querySelector("#start");
+var timer = document.querySelector(".timer");
+var secondsLeft = 60;
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timer.textContent = secondsLeft + " seconds left until the Quiz has ended.";
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+
+  setTime();
 
 //GIVEN I am taking a code quiz
 //WHEN I click the start button
