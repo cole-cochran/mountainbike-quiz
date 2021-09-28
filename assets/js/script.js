46a2381826a0ba57fcf027f65bd4cf54229e1d1b
Cole Cochran <colecochran405@gmail.com>
2021-09-27 20:17:33 -0500

    setting timer to start on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,53 @@
-//WHEN I click the start button
-var startButton = document.querySelector("#start");
+//Variables
 var timer = document.querySelector(".timer");
 var secondsLeft = 60;
+// Questions wrapped into an Array
+var mtbQuiz = [
+   { 
+    question: 'Mountain bikes were first explored as useful transportation by the military in what year?',
+    options: ['1850','1896','1920','1905'],
+    fact: "1896",
+   }      
+]
 
-function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timer.textContent = secondsLeft + " seconds left until the Quiz has ended.";
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage();
-      }
-  
-    }, 1000);
-  }
+// Function needed to hide button on click
 
+// Creates a event listener to start time on click.
+var startButton = document.querySelector("#start").onclick = function(){
+function setTime() {	
+    // Sets interval in variable	
+    var timerInterval = setInterval(function() {	
+      secondsLeft--;	
+      timer.textContent = secondsLeft + " seconds left until the Quiz has ended.";	
+  	
+      if(secondsLeft === 0) {	
+        // Stops execution of action at set interval	
+        clearInterval(timerInterval);	
+        // Calls function to create and append image	
+        displayScore();	
+      }	
+  	
+    }, 1000);	
+  }	
   setTime();
+} 
+console.log(startButton)
+
+// Trigger first question
+
+// Trigger second question
+
+// Trigger this question
+
+// Trigger fourth question
+
+// Trigger fifth question
+
+// Logic to deduct time for a wrong answer
+
+// Logic to add up score & save to local storage
+
+// Score and initials logging
 
 //GIVEN I am taking a code quiz
 //WHEN I click the start button
